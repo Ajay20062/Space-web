e2ae5ab8336a33816fc3c734cab985e0394b2935
Ajay20062 <ajaydharrsan@gmail.com>
2021-10-04 17:54:47 +0530

    3 files modified

--- ds.sql
+++ ds.sql
@@ -1,14 +1,14 @@
 -- create
-CREATE TABLE EMPLOYEE (
+CREATE TABLE Person (
   empId INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
-  dept TEXT NOT NULL
+   TEXT NOT NULL
 );
 
 -- insert
-INSERT INTO EMPLOYEE VALUES (0001, 'Clark', 'Sales', Clark@abc.com);
-INSERT INTO EMPLOYEE VALUES (0002, 'Dave', 'Accounting');
-INSERT INTO EMPLOYEE VALUES (0003, 'Ava', 'Sales');
-INSERT into EMPLOYEE VALUES (0004, 'Ajay')
+INSERT INTO Person VALUES (0001, 'Clark', 'Sales');
+INSERT INTO Person VALUES (0002, 'Dave', 'Accounting');
+INSERT INTO Person VALUES (0003, 'Ava', 'Sales');
+INSERT into Person VALUES (0004, 'Ajay')
 -- fetch 
-SELECT * FROM EMPLOYEE WHERE dept = 'Sales';
+SELECT * FROM Person WHERE dept = 'Sales';
