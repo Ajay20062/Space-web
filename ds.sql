b4d82322732e6a9e6a6aeb5aa4b8b6d4a7ae052c
T R AjayDharrsan <70842623+Ajay20062@users.noreply.github.com>
2021-10-21 19:38:10 +0530

    jak

--- ds.sql
+++ ds.sql
@@ -1,14 +1,13 @@
 -- create
 CREATE TABLE Person (
-  empId INTEGER PRIMARY KEY,
-  name TEXT NOT NULL,
-   TEXT NOT NULL
+  Number INTEGER PRIMARY KEY,
+  name TEXT NOT NULL
 );
 
 -- insert
-INSERT INTO Person VALUES (0001, 'Clark', 'Sales');
-INSERT INTO Person VALUES (0002, 'Dave', 'Accounting');
-INSERT INTO Person VALUES (0003, 'Ava', 'Sales');
+INSERT INTO Person VALUES (0001, 'Clark');
+INSERT INTO Person VALUES (0002, 'Dave');
+INSERT INTO Person VALUES (0003, 'Ava');
 INSERT into Person VALUES (0004, 'Ajay')
 -- fetch 
-SELECT * FROM Person WHERE dept = 'Sales';
+SELECT * FROM Person WHERE 
